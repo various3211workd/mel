e8e7c8d390183bf9e19d304d556684bfe0040623
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-02-24 19:38:59 +0900

    [Add] ファイルのパスで指定できるようにした

--- src/modules/catfile.rs
+++ src/modules/catfile.rs
@@ -22,30 +22,35 @@ pub fn cat_til(arg_path: String) {
     .expect("something went wrong reading the file");
   
   let init_path: Vec<&str> = contents.split("&").collect();
-
+  
   // init file path loop
   for path in init_path[0..init_path.len() - 1].into_iter() {
-    if show_path[0] == "README.md" {
+    if show_path.len() > 2 {
       let dest_path: Vec<&str> = path.rsplit("/").collect();
-      
-      if dest_path[1] == show_path[1] {
-        let mut f = File::open(path).expect("file not found");
-        let mut buf = String::new();
-        f.read_to_string(&mut buf)
-          .expect("something went wrong reading the file");
-
-        println!("{}", buf);
+      if dest_path[1] == show_path[1] && dest_path[0] == show_path[0] {
+        match show_markdown(path.to_string()) {
+          Ok(()) => {},
+          Err(e) => { panic!("{}", e) },
+        }
+        break;
       }
-      else { }
     }
     else {
-      println!("[DEBUG2] {}", path);
-      let mut f = File::open(path).expect("file not found");
-      let mut a = String::new();
-      f.read_to_string(&mut a)
-        .expect("something went wrong reading the file");
-
-      println!("{}", a);
+      match show_markdown(path.to_string()) {
+        Ok(()) => {},
+        Err(e) => { panic!("{}", e) },
+      }
+      break;
     }
   }
+}
+
+fn show_markdown(path: String) -> Result<(), String> {
+  let mut f = File::open(path).expect("file not found");
+  let mut a = String::new();
+  f.read_to_string(&mut a)
+    .expect("something went wrong reading the file");
+
+  println!("{}", a);
+  Ok(())
 }
