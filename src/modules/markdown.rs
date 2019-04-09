918774d6db5fc88d428822287bb15286f51a5eda
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-04-10 05:56:40 +0900

    [Change] markdown modules add function comment

--- src/modules/markdown.rs
+++ src/modules/markdown.rs
@@ -1,5 +1,12 @@
 use pulldown_cmark::{html, Parser};
 
+/*
+  parsing_html function
+
+  markdonw -> put html layout
+
+  @param String
+*/
 pub fn parsing_html(buf: String) {
   let parser = Parser::new(&buf);
 
@@ -9,6 +16,13 @@ pub fn parsing_html(buf: String) {
   println!("{}", html_buf);
 }
 
+/*
+  parsing_html function
+
+  markdonw -> put console layout
+  
+  @param String
+*/
 pub fn parsing(buf: String) {
 
   println!("{}", buf);
