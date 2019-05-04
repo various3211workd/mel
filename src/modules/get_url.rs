229aa8a6db1e40e7d4a8525ced23c41df9d56e15
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-05-05 05:59:24 +0900

    [Add] create get_url function and tiny changes

--- src/modules/get_url.rs
+++ src/modules/get_url.rs
@@ -1,3 +1,7 @@
+extern crate reqwest;
+use std::io;
+use std::io::Read;
+
 use super::uname::get_init_path;
 
 /*
@@ -8,4 +12,9 @@ use super::uname::get_init_path;
 pub fn get_url(url :String) {
   let init_path: String = get_init_path();
 
+  let mut resp = reqwest::get("https://github.com/various3211workd/").unwrap();
+
+  let mut s = String::new();
+  resp.read_to_string(&mut s);
+  println!("{:?}", s);
 }
