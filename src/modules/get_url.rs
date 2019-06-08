f6dbb1f617534f295e40e19e9cf08543dea4b671
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-06-09 04:01:05 +0900

    [Change] get_url function add create file and folder

--- src/modules/get_url.rs
+++ src/modules/get_url.rs
@@ -1,5 +1,7 @@
 extern crate reqwest;
 
+use std::fs::*;
+use std::io::*;
 use std::io::Read;
 
 use super::uname::get_init_path;
@@ -10,23 +12,32 @@ use super::edit_til::write_markdown;
 
   @param url String
 */
-pub fn get_url(url: String, filepath: String) {
+pub fn get_url(url: String, filepath: String) -> Result<()> {
   
+  const INIT_PATH_LEN: usize = 3;
   let mut init_path: String;
 
   // path first elements is '/'
-  if filepath.chars().nth(0) == Some('/') {
-    init_path = get_init_path() + &filepath;
+  if filepath.chars().nth(0) != Some('/') {
+    init_path = "/".to_string() + &filepath;
   }
   else {
-    init_path = get_init_path() + "/" + &filepath;
+    init_path = filepath.to_string();
   }
   
-  //let mut resp = reqwest::get("https://google.com").unwrap();
+  let init_path_vec: Vec<&str> = init_path.split("/").collect();
+  if init_path_vec.len() >= INIT_PATH_LEN {
+    create_dir_all(get_init_path() + &init_path.replace(init_path_vec[init_path_vec.len() - 1], ""))?;
+  }
+  
+  init_path = get_init_path() + &init_path;
 
-  //let mut s = String::new();
-  //resp.read_to_string(&mut s);
+  let mut resp = reqwest::get("https://google.com").unwrap();
+
+  let mut s = String::new();
+  resp.read_to_string(&mut s)?;
   
   write_markdown(init_path, s);
 
+  Ok(())
 }
